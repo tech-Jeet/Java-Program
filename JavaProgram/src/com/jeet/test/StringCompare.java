cbcbb56bdc90b7c71c112268c77b6c7f9e547a8a
Vishvajeet <vishvajeetgupta100@gmail.com>
2021-02-08 08:58:39 +0530

    added StringCompare

--- JavaProgram/src/com/jeet/test/StringCompare.java
+++ JavaProgram/src/com/jeet/test/StringCompare.java
@@ -1,5 +1,26 @@
 package com.jeet.test;
 
+import java.util.Scanner;
+
 public class StringCompare {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your String");
+		String str = sc.nextLine();
+		System.out.println("enter sub Sring");
+		String subString = sc.next();
+		
+		String[] strArray = str.split(" ");
+		boolean flag = false;
+		
+		for(String str1 : strArray) {
+			if(str1.equalsIgnoreCase(subString)) {
+				flag = true;
+			}
+		}
+		System.out.println("sub String present in String :: "+ flag);
+	}
 
 }
